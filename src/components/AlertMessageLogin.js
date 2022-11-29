d3cf197839c3f61316ef41c98a462196bfb92795
DatChau <119236124+DatChau@users.noreply.github.com>
2022-11-29 23:01:32 +0700

    ok

--- src/components/AlertMessageLogin.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Alert from "react-bootstrap/Alert";
-const AlertMessageLogin = ({ info }) => {
-  return info === null ? null : (
-    <Alert style={{ color: "red" }}>{info.message}</Alert>
-  );
-};
-
-export default AlertMessageLogin;
